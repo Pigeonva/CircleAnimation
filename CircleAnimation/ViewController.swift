4eecd245ad0c1eace867b262803a482cc2aa78b4
Pigeonva <77341488+Pigeonva@users.noreply.github.com>
2022-09-21 17:32:50 +0300

    feat: create Direction enum, add IBOutlet, variables, IBActions.

--- CircleAnimation/ViewController.swift
+++ CircleAnimation/ViewController.swift
@@ -7,13 +7,44 @@
 
 import UIKit
 
+//MARK: - Direction enum
+
+enum Direction {
+    case down
+    case right
+    case up
+    case left
+}
+
 class ViewController: UIViewController {
+    
+    //MARK: - IBOutlets
+    
+    @IBOutlet weak var startButton: UIButton!
+    
+    //MARK: - var/let
+    
+    var circle = UIView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+    var direction: Direction = .down
+    var condition = false
+    
+    //MARK: - Lifecycle methods
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    //MARK: - IBActions
+    
+    @IBAction func startPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func stopPressed(_ sender: UIButton) {
+        
+    }
+    
+    //MARK: - functions
 }
 
